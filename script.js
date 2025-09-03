ddda98b6fcf2b2e3d3689f378807f691b244749b
HARTK850 <y15761576@gmail.com>
2025-09-03 17:26:37 +0300

    Update script.js

--- script.js
+++ script.js
@@ -128,8 +128,8 @@ class StoryGenerator {
     }
 
     try {
-      const response = await fetch(
-        https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${apiKey},
+      const response = await fetch
+        ( https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${apiKey},
         {
           method: "POST",
           headers: { "Content-Type": "application/json" },
